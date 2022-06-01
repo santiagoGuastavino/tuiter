26afea3b8810b53820095233932d9eb4f081004a
Santiago Guastavino <san.guasta@gmail.com>
2022-06-01 11:52:21 -0300

    trying new deploy

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin')
 
-const serviceAccount = require('./firebase-keys.json')
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEYS)
 
 if (!admin.apps.length) {
   admin.initializeApp({
